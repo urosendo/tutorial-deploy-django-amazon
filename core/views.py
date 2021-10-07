6389cb9d921a0ddd7f97f61687c0de050d872d28
Ullysses <urozendo@gmail.com>
2021-10-07 00:38:38 -0300

    Mudança de chave de instância

--- core/views.py
+++ core/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     template_name = 'core/index.html'
-    message = 'Aplicação Django no ar utilizando uWSGI + NGinx'
+    message = 'Deploy de aplicação Django utilizando uWSGI + NGinx'
     return render(request, template_name, {'message':message})
